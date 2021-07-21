6b4db218f4ad4952d431bfd65ebbc17cc5768947
Adam SungMin Park <parksm2@uci.edu>
2021-07-21 20:30:03 +0000

    react app set up

---
+++ todo-list/src/components/todoList.js
@@ -0,0 +1,11 @@
+
+
+
+export default function TodoList(props){
+  return (
+      <>
+        <h1>todo list will go here</h1>
+      </>
+
+  )
+}
---
+++ todo-list/src/components/todoListEdit.js
@@ -0,0 +1,11 @@
+
+
+
+export default function TodoListEdit(props){
+  return (
+      <>
+        <h1>todo list edition will go here</h1>
+      </>
+
+  )
+}
